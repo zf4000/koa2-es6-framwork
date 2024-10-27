31092bdfc28d7e861ce42e9b77c9b531efc6e0ed
zf4000 <jeff@eqinfo.com.cn>
2024-10-27 10:30:31 +0800

    fix: 完善了代码

--- config/default.config.js
+++ config/default.config.js
@@ -1,5 +1,5 @@
 export default {
-  disableLogger: false,
-  enableSocket: true,
-  enableCron: true,
+  disableLogger: false, //是否启用logger
+  enableSocket: true, //是否启用socket.io
+  enableCron: true, //是否启用cron
 };
